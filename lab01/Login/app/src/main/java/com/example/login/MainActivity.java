3b85e13e52c772f255b60540a90a36a86d3f1a2b
Mai Trang <116065556+BanhMiRuoc@users.noreply.github.com>
2024-10-02 11:24:03 +0700

    Add files via upload

---
+++ lab01/Login/app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.login;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+    private CheckBox rememberMeCheckBox;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
